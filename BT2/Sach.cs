8f41d59ae52a3899919ae108f09da691ecc123c8
Quoc <lamquoca3k12@gmail.com>
2023-09-22 08:51:01 +0700

    Add project files.

---
+++ BT2/Sach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    internal class Sach
+    {
+        public int masach {  get; set; }
+        public String  tensach { get; set; }
+        public String tacgia {  get; set; }
+        public Sach() { 
+           
+        }
+        public Sach(int ms, string ts, string tg)
+        {
+            masach = ms;
+            tensach = ts;
+            tacgia = tg;
+        }
+    }
+}
---
+++ BT2/ThuVien.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    internal class ThuVien
+    {
+        Sach s = new Sach();
+        List<Sach> ds = new List<Sach>();
+        
+        public void themsach() { 
+           
+            Console.WriteLine("Nhap so luong sach :");
+            int n=Convert.ToInt32(Console.ReadLine());  
+            for (int i = 0; i < n; i++)
+            {
+              
+                Console.WriteLine("Nhap sach "+(i+1)+" : ");
+                Console.Write("Nhap ma:");
+                s.masach=Convert.ToInt32(Console.ReadLine());
+                Console.Write("Nhap ten:");
+                s.tensach = Console.ReadLine();
+                Console.Write("Nhap tac gia:");
+                s.tacgia = Console.ReadLine();
+
+
+
+            }
+        }
+        public void htsach()
+        {
+            Console.WriteLine("Hien thi"); 
+            foreach (Sach s in ds)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            ThuVien tv = new ThuVien();
+            Console.ReadKey();
+        }
+        
+    }
+}
